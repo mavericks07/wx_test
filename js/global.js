7ac9a7fac8f95c8eb0115ad89e463fae5423a890
mavericks <czh6030@163.com>
2017-07-25 09:51:19 +0800

    init

---
+++ js/global.js
@@ -0,0 +1,9 @@
+﻿
+$(function(){
+	
+	//适配屏幕大小
+	var iWidth=document.documentElement.getBoundingClientRect().width;
+		iWidth=iWidth>750?750:iWidth;
+		iWidth=iWidth<320?320:iWidth;
+		document.getElementsByTagName("html")[0].style.fontSize=iWidth/7.5+"px";
+})
